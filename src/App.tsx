0b41818ff832d94cb97e393c1b6dc456aa0fef23
guilhermedeandrade <guilherme.de.andrade@viz.ai>
2022-10-14 11:59:49 +0100

    chore: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -17,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
