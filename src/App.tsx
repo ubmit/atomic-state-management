9d347a921e2c17783d26514f241b51eb673d121b
guilhermedeandrade <guilherme.de.andrade@viz.ai>
2022-10-17 10:26:55 +0100

    feat: add routing Add react-location package to handle routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {Outlet, ReactLocation, Router} from '@tanstack/react-location'
+
+import {Home} from './home'
 import {RecoilCharacterCounter} from './examples/recoil'
 
+const location = new ReactLocation()
+
+const routes = [
+  {path: '/', element: <Home />},
+  {path: 'recoil', element: <RecoilCharacterCounter />},
+]
+
 function App() {
-  return <RecoilCharacterCounter />
+  return (
+    <Router location={location} routes={routes}>
+      <Outlet />
+    </Router>
+  )
 }
 
 export default App
